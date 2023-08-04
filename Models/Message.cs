c65bfd26d337589dc475d3476b5facc1c0fea221
EtienneT64 <110019770+EtienneT64@users.noreply.github.com>
2023-08-04 13:50:50 +0200

    Added a comment

---
+++ Models/Message.cs
@@ -0,0 +1,13 @@
+﻿namespace ChatRooms.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public int MsgLength { get; set; }
+        public DateTime SendDate { get; set; }
+
+        public User User { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatRooms.Models
+{
+    //public enum Status
+    //{
+    //    Guest, Member, Admin
+    //}
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, StringLength(30)]
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string DisplayName { get; set; }
+        public string DisplayNameColor { get; set; }
+        public DateTime RegisterDate { get; set; }
+
+        //public Status? Status { get; set; }
+
+        public ICollection<Message> Messages { get; set; }
+
+        // testing git
+    }
+}
