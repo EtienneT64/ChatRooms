4e9e9b31b36a2bf968ecfa3b98e607209a9799d1
EtienneT64 <110019770+EtienneT64@users.noreply.github.com>
2023-08-06 16:55:02 +0200

    migrated, updatead, and seeded database

---
+++ Migrations/20230806143700_InitialV2.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatRooms.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialV2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Chatrooms_ChatroomId",
+                table: "Messages");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ChatroomId",
+                table: "Messages",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Chatrooms_ChatroomId",
+                table: "Messages",
+                column: "ChatroomId",
+                principalTable: "Chatrooms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Chatrooms_ChatroomId",
+                table: "Messages");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ChatroomId",
+                table: "Messages",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Chatrooms_ChatroomId",
+                table: "Messages",
+                column: "ChatroomId",
+                principalTable: "Chatrooms",
+                principalColumn: "Id");
+        }
+    }
+}
