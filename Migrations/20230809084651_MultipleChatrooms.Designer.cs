f10b9769bfa8e01b2b91144c61f8d94894ca1295
EtienneT64 <110019770+EtienneT64@users.noreply.github.com>
2023-08-09 10:47:00 +0200

    created new migration

--- Migrations/20230809084651_MultipleChatrooms.Designer.cs
+++ Migrations/20230809084651_MultipleChatrooms.Designer.cs
@@ -4,6 +4,7 @@ using ChatRooms.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ChatRooms.Migrations
 {
     [DbContext(typeof(ChatroomContext))]
-    partial class ChatroomContextModelSnapshot : ModelSnapshot
+    [Migration("20230809084651_MultipleChatrooms")]
+    partial class MultipleChatrooms
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
