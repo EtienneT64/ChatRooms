e9533fea7c58706a6cceede93741d49351db30b7
EtienneT64 <110019770+EtienneT64@users.noreply.github.com>
2023-08-09 11:32:14 +0200

    modified user model

--- Models/User.cs
+++ Models/User.cs
@@ -1,27 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace ChatRooms.Models
 {
 
-    public class User
+    public class User : IdentityUser
     {
-        [Key]
-        public int Id { get; set; }
-
-        [Required, StringLength(30), MinLength(2)]
-        public string Username { get; set; }
-
-        [Required, DataType(DataType.Password)]
-        public string Password { get; set; }
-
         [Required, StringLength(30), Display(Name = "Display Name")]
         public string DisplayName { get; set; }
 
         [Required, StringLength(7)]
         public string DisplayNameColor { get; set; }
-
-        [DataType(DataType.DateTime)]
-        public DateTime RegisterDate { get; set; }
 
         public int? ChatroomId { get; set; }
 
