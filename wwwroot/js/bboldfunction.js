4ca5c7eb4546ff5817d4453a7e670cc786fac7da
Liam Venter <liamventer460@gmail.com>
2023-08-14 11:20:21 +0200

    Bold function experimenting

---
+++ wwwroot/js/bboldfunction.js
@@ -0,0 +1,16 @@
+﻿<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        const messageContent = document.getElementById("messageContent");
+        const bold = document.getElementById("bold");
+
+    bold.addEventListener("click", function () {
+            const start = messageContent.selectionStart;
+    const end = messageContent.selectionEnd;
+
+    const selectedText = messageContent.value.substring(start, end);
+    const modifiedText = `<b>${selectedText}</b>`;
+
+    messageContent.setRangeText(modifiedText, start, end, "select");
+        });
+    });
+</script>
