1db0573a7750395563bc36c04bbb2b84530f561c
EtienneT64 <110019770+EtienneT64@users.noreply.github.com>
2023-08-18 19:55:37 +0200

    added experimental geo ipinfo code

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,23 +1,57 @@
-﻿using ChatRooms.Models;
+﻿using ChatRooms.Helpers;
+using ChatRooms.Interfaces;
+using ChatRooms.Models;
+using ChatRooms.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Globalization;
+using System.Net;
 
 namespace ChatRooms.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUserRepository _userRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUserRepository userRepository)
         {
             _logger = logger;
+            _userRepository = userRepository;
         }
 
         [Authorize]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var ipInfo = new IPInfo();
+            var homeViewModel = new HomeViewModel();
+            try
+            {
+                string url = "https://ipinfo.io?token=38eefb25a8168b";
+                var info = new WebClient().DownloadString(url);
+                ipInfo = JsonConvert.DeserializeObject<IPInfo>(info);
+                RegionInfo myRI1 = new RegionInfo(ipInfo.Country);
+                ipInfo.Country = myRI1.EnglishName;
+                homeViewModel.City = ipInfo.City;
+                homeViewModel.State = ipInfo.Region;
+                if (homeViewModel.City != null)
+                {
+                    homeViewModel.Users = await _userRepository.GetAllUsers(); // add city to user model
+                }
+                else
+                {
+                    homeViewModel.Users = null;
+                }
+                return View(homeViewModel);
+            }
+            catch (Exception ex)
+            {
+                homeViewModel.Users = null;
+            }
+
+            return View(homeViewModel);
         }
 
         public IActionResult Privacy()
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using ChatRooms.Models;
+
+namespace ChatRooms.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<User> Users { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
