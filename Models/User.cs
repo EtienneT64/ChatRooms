4766fd5e95c4616dee86d660209dbc5131d49390
EtienneT64 <110019770+EtienneT64@users.noreply.github.com>
2023-08-23 12:09:50 +0200

    added default profile picture to user model

--- Models/User.cs
+++ Models/User.cs
@@ -4,7 +4,7 @@ namespace ChatRooms.Models
 {
     public class User : IdentityUser
     {
-        public string? ProfileImageUrl { get; set; }
+        public string? ProfileImageUrl { get; set; } = "http://res.cloudinary.com/dzjsiibch/image/upload/v1692785238/h1jgoxervta83v5xpljo.png";
         public ICollection<Message>? Messages { get; set; }
         public ICollection<Chatroom>? Chatrooms { get; set; }
         public ICollection<UserPinnedChatroom>? PinnedChatrooms { get; set; }
