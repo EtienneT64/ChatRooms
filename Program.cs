247fd0aca6d7557b0ebadb36b284e1f5c6f9eab6
EtienneT64 <110019770+EtienneT64@users.noreply.github.com>
2023-08-25 17:13:49 +0200

    updated program.cs

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddScoped<IChatroomService, ChatroomService>();
 builder.Services.Configure<CloudinarySettings>(builder.Configuration.GetSection("CloudinarySettings"));
 builder.Services.AddDbContext<ChatroomContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ChatRoomsContextProd"));
 });
 builder.Services.AddIdentity<User, IdentityRole>()
     .AddEntityFrameworkStores<ChatroomContext>();
